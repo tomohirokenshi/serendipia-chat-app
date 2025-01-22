fff974017bbe01f3706b98ee69297781bf57d0dd
Timothy Kenneth D. Dantic <timothykennethdantic@gmail.com>
2025-01-22 22:35:13 +0800

    Minor Changes

--- frontend/src/components/skeletons/SidebarSkeleton.jsx
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -5,13 +5,13 @@ const SidebarSkeleton = () => {
 
   return (
     <aside
-      className="h-full w-20 lg:w-72 border-r border-base-300 
+      className="h-full w-full lg:w-72 border-r border-base-300 
     flex flex-col transition-all duration-200"
     >
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="w-6 h-6" />
-          <span className="font-medium hidden lg:block">Contacts</span>
+          <span className="font-medium block">Contacts</span>
         </div>
       </div>
       <div className="overflow-y-auto w-full py-3">
@@ -20,7 +20,7 @@ const SidebarSkeleton = () => {
             <div className="relative mx-auto lg:mx-0">
               <div className="skeleton size-12 rounded-full" />
             </div>
-            <div className="hidden lg:block text-left min-w-0 flex-1">
+            <div className="block text-left min-w-0 flex-1">
               <div className="skeleton h-4 w-32 mb-2" />
               <div className="skeleton h-3 w-16" />
             </div>
